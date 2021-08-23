8b2179f3f719ee9801165eb8adafd54d23ccdd10
leandrofacim <leandro.facim@wivenn.com.br>
2021-08-23 20:47:48 -0300

    layouts

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ChatController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('chat', [ChatController::class, 'index'])->name('chat');
+
+Route::redirect('/', 'chat')->name('home');
 
 Auth::routes();
 
