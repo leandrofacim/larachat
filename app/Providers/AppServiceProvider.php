3259192a695a23d76fb9d75e033f14fc66c80710
leandrofacim <leandro.facim@wivenn.com.br>
2022-02-10 09:33:49 -0300

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(767);
+        Schema::defaultStringLength(191);
     }
 }
